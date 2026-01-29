6eb090082cb1472a4bcd55a3e2327e5e8ce4259a
antoniomacedo1 <antoniofilipe.thomazmacedo@edu.sait.ca>
2026-01-28 23:39:24 -0700

    adding the home screen and some features

--- apps/frontend/app/index.tsx
+++ apps/frontend/app/index.tsx
@@ -1,5 +1,12 @@
 import { Redirect } from "expo-router";
 
 export default function Index() {
-  return <Redirect href="/login" />;
+  const isAuthenticated = false;
+
+  return isAuthenticated ? (
+    <Redirect href="/(tabs)/home" />
+  ) : (
+    <Redirect href="/login" />
+  );
 }
+
